2da39ac3ca0ba8a22582d4579c9c579e1daceed8
Andre Hardika <andrehardika@gmail.com>
2017-01-24 16:49:54 +0700

    Add migrations

---
+++ database/migrations/2017_01_24_092332_create_owners_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            $table->string('id', 20)->primary();
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->text('address');
+            $table->enum('gender', ['0', '1']);
+            $table->date('birthdate');
+            $table->string('phone', 20);
+            $table->timestamps();
+            $table->integer('created_by')->unsigned()->index();
+            $table->integer('updated_by')->unsigned()->index();
+            $table->integer('deleted_by')->unsigned()->index();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owners');
+    }
+}
---
+++ database/migrations/2017_01_24_092953_create_cafe_branches_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCafeBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cafe_branches', function (Blueprint $table) {
+            $table->string('id', 20)->primary();
+            $table->string('cafe_id', 20)->index();
+            $table->string('city');
+            $table->text('address');
+            $table->string('phone', 20);
+            $table->string('open_hours', 10);
+            $table->string('close_hours', 10);
+            $table->timestamps();
+            $table->integer('created_by')->unsigned()->index();
+            $table->integer('updated_by')->unsigned()->index();
+            $table->integer('deleted_by')->unsigned()->index();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cafe_branches');
+    }
+}
