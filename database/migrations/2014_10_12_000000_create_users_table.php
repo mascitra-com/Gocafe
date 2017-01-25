b38ccd8efc9b1aca31954cfa26195dc4ec6c2633
Andre Hardika <andrehardika@gmail.com>
2017-01-25 14:51:31 +0700

    Modified users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->enum('level', ['0', '1']); //0 = Owner, 1= Staff
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
