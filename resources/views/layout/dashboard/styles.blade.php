b75f1c1d4950fd9d53fc4738ad4e278276dba5a2
Ainul Yakin <ainul086@gmail.com>
2017-01-25 16:17:19 +0700

    Inisialisasi UI dashboard

---
+++ resources/views/layout/dashboard/styles.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="assets/css/theme-dashboard.css">
+<link rel="stylesheet" href="assets/css/theme-helper.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Roboto:300,400" rel="stylesheet">
