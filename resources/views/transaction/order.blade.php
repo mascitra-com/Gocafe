d008c8ae3e2265150cec13c17ce7bcf6d83b944c
Rizki Herdatullah <rizkiherda@gmail.com>
2017-04-21 14:30:45 +0700

    Update UI for Transaction

--- resources/views/transaction/order.blade.php
+++ resources/views/transaction/order.blade.php
@@ -19,18 +19,19 @@
             </div>
             <div class="row grid2">
                 <div class="list" id="product">
-                    @foreach($menus as $menu)<button class="rectangle product"><img src="{{ url("menus/showThumbnail/$menu->id")}}" alt="Thumbnail">{{ $menu->name }}</button>@endforeach
+                    @foreach($menus as $menu)<button class="rectangle product" onclick="getProductDetail('{{ $menu->id }}')"><img src="{{ url("menus/showThumbnail/$menu->id")}}" alt="Thumbnail">{{ $menu->name }}</button>@endforeach
                 </div>
             </div>
         </div>
-        <div class="col-md-6" style="overflow:auto; position:absolute; top:0; left:50%; right:0px; bottom:50px;" id="productDetail">
+        <div class="col-md-6" style="overflow:auto; position:absolute; top:0; left:50%; right:0px; bottom:50px; z-index: 0" id="productDetail">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title pull-left">Detail Produk</h3>
+                    <div class="pull-right" id="btn-add"><button class="btn btn-primary" onclick="addToCheck('{{ $firstMenu->id }}')"><i class="fa fa-plus"></i></button></div>
                     <div class="clearfix"></div>
                 </div>
                 <div class="panel-body">
-                    <h3 class="head-menu">{{ $firstMenu->name }}</h3>
+                    <h3 id="head-menu">{{ $firstMenu->name }}</h3>
                     <div>
                         <i class="fa fa-star text-primary"></i>
                         <i class="fa fa-star text-primary"></i>
@@ -42,7 +43,7 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="thumbnail">
-                                <img src="{{ url("menus/showThumbnail/$firstMenu->id")}}" alt="Thumbnail">
+                                <img style="max-height: 185px" src="{{ url("menus/showThumbnail/$firstMenu->id")}}" alt="Thumbnail" id="big-thumbnail">
                             </div>
                         </div>
                         <div class="col-md-6 table-responsive detail-menu">
@@ -50,11 +51,11 @@
                             <table class="table">
                                 <tr>
                                     <td class="text-primary">Harga</td>
-                                    <td>Rp. {{ number_format($firstMenu->price, 0, ',', '.') }},-</td>
+                                    <td id="price">Rp. {{ number_format($firstMenu->price, 0, ',', '.') }},-</td>
                                 </tr>
                                 <tr>
                                     <td class="text-primary">Diskon</td>
-                                    <td>@if($firstMenu->discount)
+                                    <td id="discount">@if($firstMenu->discount)
                                         - Rp. {{ number_format($firstMenu->price * $firstMenu->discount, 0, ',', '.') }},-
                                         @else
                                             -
@@ -64,29 +65,60 @@
                             </table>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row" id="thumbnails">
                         @for($i = 0; $i < 4; $i++)
                             <div class="col-xs-6 col-md-3">
-                                <a href="#" class="thumbnail">
+                                <button class="thumbnail">
                                     <img src="{{ url("menus/showThumbnail/MCFDK120170419082141")}}" alt="Thumbnail">
-                                </a>
+                                </button>
                             </div>
                         @endfor
                     </div>
                     <div class="row col-md-12">
                         <h4>Deskripsi :</h4><br>
-                        <p class="text-primary">{{ $firstMenu->description }}</p><br>
+                        <p class="text-primary" id="menu-desc">{{ $firstMenu->description }}</p><br>
                     </div>
                     <div class="row col-md-12">
                         <h4>Ulasan :</h4><br>
+                        {{-- TODO Use Ajax Instead --}}
+                        <form action="{{ url('review') }}" method="post">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="item_id" value="{{ $firstMenu->id }}" id="item_id">
+                            <table class="table table-responsive">
+                                <tr>
+                                    <td>Penilaian Anda</td>
+                                    <td>
+                                        <div>
+                                            <i class="fa fa-star text-primary"></i>
+                                            <i class="fa fa-star text-primary"></i>
+                                            <i class="fa fa-star text-primary"></i>
+                                            <i class="fa fa-star text-primary"></i>
+                                            <i class="fa fa-star-half-o text-primary"></i>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Ulasan Anda</td>
+                                    <td><textarea name="review" class="form-control" id="review" rows="5"></textarea></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        <button class="btn btn-primary pull-right">Simpan</button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                    <div class="row col-md-12">
                         <table class="table table-responsive">
                             @for($i = 0; $i < 4; $i++)
                             <tr>
                                 <td width="15%">
                                     <img src="{{ asset('images/blank-avatar.png') }}" alt="" class="img-circle img-responsive">
                                 </td>
-                                <td class="{{ $i % 2 === 0 ?: 'text-primary' }}">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, culpa cumque, debitis dolor exercitationem hic impedit incidunt ipsum iure laboriosam laborum molestias non, porro praesentium quasi repudiandae sequi tempora velit?
+                                <td>
+                                    <p style="font-size: 14pt" class="text-primary">Lorem Ipsum</p>
+                                    Animi, culpa cumque, debitis dolor exercitationem hic impedit incidunt ipsum iure laboriosam laborum molestias non, porro praesentium quasi repudiandae sequi tempora velit? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                                 </td>
                             </tr>
                             @endfor
@@ -104,7 +136,7 @@
             font-size: 12pt;
         }
 
-        .head-menu {
+        #head-menu {
             margin-bottom: .5em;
         }
 
@@ -134,7 +166,7 @@
             background-image: linear-gradient(0deg, rgba(140, 71, 40, 0.75) 25%, rgba(140, 71, 40, 1) 25%, rgba(140, 71, 40, 0.8) 90%);
             height: 100px;
             width: 90px;
-            border: 0.5px solid #7c4621;
+            border: 1px solid #7c4621;
             border-radius: 5px;
             color: white;
             text-align: center;
@@ -161,9 +193,8 @@
             line-height:20px;
         }
 
-        ul.nav li a {
+        #productList ul.nav li a {
             color: whitesmoke;
-            pointer-events: none;
             cursor: default;
         }
 
@@ -189,10 +220,18 @@
             cursor: pointer;
             overflow: hidden;
         }
+
+        button.deleteMenu {
+            background: Transparent no-repeat;
+            border: none;
+            cursor: pointer;
+            overflow: hidden;
+        }
     </style>
 @endsection
 
 @section('javascripts')
     <script src="{{ url('plugins/jquery/jquery.number.min.js') }}"></script>
+    <script src="{{ url('js/payment.js') }}"></script>
     <script src="{{ url('js/order.js') }}"></script>
 @endsection
