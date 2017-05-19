edf33193746099edd4a93d309f5a644ade6bd013
Rizki Herdatullah <rizkiherda@gmail.com>
2017-05-19 14:20:59 +0700

    Add Footer to Product Detail at Customer View

--- resources/views/homepage/product.blade.php
+++ resources/views/homepage/product.blade.php
@@ -4,7 +4,7 @@
     <div class="ui grid container">
         {{-- Main Content --}}
         <div class="twelve wide column">
-            <h1>{{ $product->name }}</h1>
+            <h1 style="color: #8C4728">{{ $product->name }}</h1>
             <div class="ui segment">
                 Bagikan :
                 <button class="ui facebook button">
@@ -62,7 +62,7 @@
         {{-- Sidebar --}}
         <div class="four wide column">
             <div class="ui center aligned segment">
-                <h2>Rp. {{ number_format($product->price, 0, ',', '.') }},-</h2>
+                <h2 style="color: #8C4728">Rp. {{ number_format($product->price, 0, ',', '.') }},-</h2>
                 <p class="price-last-update">Perubahan Harga Terakhir: {{ date('d-m-Y, H:i') }}</p>
             </div>
             <button class="ui fluid button"><i class="fa fa-heart"></i> &nbsp Tambah ke Wishlist</button>
@@ -78,6 +78,34 @@
                 <button class="ui fluid button"><i class="fa fa-envelope"></i> &nbsp Kirim Pesan</button>
             </div>
         </div>
+    </div><br>
+    {{--Footer--}}
+    <div class="ui vertical footer segment">
+        <div class="ui container">
+            <div class="ui stackable divided equal height stackable grid">
+                <div class="five wide column">
+                    <h4 class="ui header">About</h4>
+                    <div class="ui link list">
+                        <a href="#" class="item">Sitemap</a>
+                        <a href="#" class="item">Contact Us</a>
+                        <a href="#" class="item">Religious Ceremonies</a>
+                        <a href="#" class="item">Gazebo Plans</a>
+                    </div>
+                </div>
+                <div class="six wide column">
+                    <h4 class="ui header">Services</h4>
+                    <div class="ui link list">
+                        <a href="#" class="item">Banana Pre-Order</a>
+                        <a href="#" class="item">DNA FAQ</a>
+                        <a href="#" class="item">How To Access</a>
+                        <a href="#" class="item">Favorite X-Men</a>
+                    </div>
+                </div>
+                <div class="five wide column">
+                    <h4 class="ui header">Footer Header</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab beatae dolorem itaque neque praesentium?</p>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
