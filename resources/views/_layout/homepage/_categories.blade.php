6c4cd1d2217a8af0b511694dd0a6f5fc76c5f7d5
LAPTOP/DILA <dilarisma21@gmail.com>
2017-06-21 13:03:26 +0700

    Perubahan

--- resources/views/_layout/homepage/_categories.blade.php
+++ resources/views/_layout/homepage/_categories.blade.php
@@ -7,34 +7,105 @@
         <i class="dropdown icon"></i>
         <span class="text">Masakan Indonesia</span>
         <div class="menu">
-          <div class="item">ACEH</div>
-          <div class="item">SUMATERA UTARA</div>
-          <div class="item">SUMATERA BARAT</div>
-          <div class="item">RIAU</div>
-          <div class="item">JAMBI</div>
-          <div class="item">SUMATERA SELATAN</div>
-          <div class="item">BENGKULU</div>
-          <div class="item">LAMPUNG</div>
-          <div class="item">KEPULAUAN BANGKA BELITUNG</div>
-          <div class="item">KEPULAUAN RIAU</div>
-          <div class="item">DKI JAKARTA</div>
-          <div class="item">JAWA BARAT</div>
-          <div class="item">JAWA TENGAH</div>
-          <div class="item">DI YOGYAKARTA</div>
-          <div class="item">JAWA TIMUR</div>
-          <div class="item">BANTEN</div>
+          <div class="item">
+          <i class="dropdown icon"></i>
+          <span class="text">Sumattra</span>
+            <div class="menu">
+            <div class="item">Aceh</div>
+            <div class="item">Sumatra Utara</div>
+            <div class="item">Sumatra Barat</div>
+            <div class="item">Riau</div>
+            <div class="item">Jambi</div>
+            <div class="item">Sumatra Selatan</div>
+            <div class="item">Bengkulu</div>
+            <div class="item">Lampung</div>
+            <div class="item">Kepulauan Bangka Belitung</div>
+            <div class="item">Kepulauan Riau</div>
+            </div>
           </div>
+
+          <div class="item">
+          <i class="dropdown icon"></i>
+          <span class="text">Jawa</span>
+            <div class="menu">
+            <div class="item">DKI Jakarta</div>
+            <div class="item">Jawa Barat</div>
+            <div class="item">Jawa Tengah</div>
+            <div class="item">DI Yogyakarta</div>
+            <div class="item">Jawa Timur</div>
+            <div class="item">
+            <i class="dropdown icon"></i>
+            <span class="text">Banten</span>
+                <div class="menu">
+                <div class="item">Makanan</div>
+                </div>
+            </div>
+            </div>
+          </div>
+
+          <div class="item">
+          <i class="dropdown icon"></i>
+          <span class="text">Nusa Tenggara</span>
+            <div class="menu">
+            <div class="item">Bali</div>
+            <div class="item">Nusa Tenggara Barat</div>
+            <div class="item">Nusa Tenggara Timur</div>
+            </div>
+          </div>
+
+          <div class="item">
+          <i class="dropdown icon"></i>
+          <span class="text">Kalimantan</span>
+            <div class="menu">
+            <div class="item">Kalimantan Barat</div>
+            <div class="item">Kalimantan Tengah</div>
+            <div class="item">Kalimantan Selatan</div>
+            <div class="item">Kalimantan Timur</div>
+            <div class="item">Kalimantan Utara</div>
+            </div>
+          </div>
+
+          <div class="item">
+          <i class="dropdown icon"></i>
+          <span class="text">Sulawesi</span>
+            <div class="menu">
+            <div class="item">Sulawesi Utara</div>
+            <div class="item">Sulawesi Tengah</div>
+            <div class="item">Sulawesi Selatan</div>
+            <div class="item">Sulawesi Tenggara</div>
+            <div class="item">Gorontalo</div>
+            <div class="item">Sulawesi Barat</div>
+            </div>
+          </div>
+
+          <div class="item">
+          <i class="dropdown icon"></i>
+          <span class="text">Maluku</span>
+            <div class="menu">
+            <div class="item">Maluku</div>
+            <div class="item">Maluku Utara</div>
+            </div>
+          </div>
+
+          <div class="item">
+          <i class="dropdown icon"></i>
+          <span class="text">Papua</span>
+            <div class="menu">
+            <div class="item">Papua</div>
+            <div class="item">Papua Barat</div>
+            </div>
+          </div>
+        </div>
         </div>
 
         <div class="item">
         <i class="dropdown icon"></i>
         <span class="text">Masakan Korea</span>
         <div class="menu">
-          <div class="header">Mens</div>
-          <div class="item">Shirts</div>
-          <div class="item">Pants</div>
-          <div class="item">Jeans</div>
-          <div class="item">Shoes</div>
+          <div class="item">Dessert</div>
+          <div class="item">Minuman</div>
+          <div class="item">Makanan</div>
+          <div class="item">Makanan Ringan</div>
           </div>
         </div>
     </div>
