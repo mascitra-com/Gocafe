dac00f36218267f36641d7762da00e7496255537
Rizki Herdatullah <rizkiherda@gmail.com>
2019-01-14 13:13:37 +0700

    Synchronize with server

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,10 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.10/semantic.min.css">
     <link href="/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/homepage.css') }}">
 
     <script>
         window.Laravel = <?php echo json_encode([
